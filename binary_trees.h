150091729088e94ebaf714ce831ca4cfc04609a5
Raghad717 <r4gh4dalmalki@gmail.com>
2025-12-10 11:30:55 +0000

    Add header file with binary tree structure and prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -13,28 +14,31 @@
  */
 typedef struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 } binary_tree_t;
 
+/* Typedef for binary tree node */
+typedef struct binary_tree_s binary_tree_t;
+
+/* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
-void binary_tree_print(const binary_tree_t *tree);
-/* Function prototypes */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_delete(binary_tree_t *tree);
-int binary_tree_is_leaf(const binary_tree_t *node);
-int binary_tree_is_root(const binary_tree_t *node);
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+/* Function prototype for binary_tree_inorder */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_print(const binary_tree_t *tree);
-size_t binary_tree_height(const binary_tree_t *tree);
 
-#endif /* BINARY_TREES_H */ 
+/* Helper function to create a new node (used in main) */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* Function to print binary tree (provided separately) */
+void binary_tree_print(const binary_tree_t *tree);
+
+#endif /* BINARY_TREES_H */
