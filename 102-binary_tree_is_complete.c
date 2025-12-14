f24b8bc6b273ba1ba38bcd79d68346f46b0ffdad
aasiria24 <amaalmoasiri@gmail.com>
2025-12-14 19:53:34 +0000

    Fixing the amount of lines

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,66 +1,51 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
 * binary_tree_is_complete - Checks if a binary tree is complete
-* @tree: Pointer to the root node of the tree to check
+* @tree: Pointer to the root node
 *
-* Return: 1 if the tree is complete, 0 otherwise
-*         If tree is NULL, return 0
+* Return: 1 if complete, 0 otherwise
 */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-const binary_tree_t **queue;
-int front = 0, rear = 0;
-int flag = 0; /* Flag to indicate if we've seen a non-full node */
-const binary_tree_t *current;
+binary_tree_t **queue;
+int front = 0, rear = 0, flag = 0, i, count = 0;
+const binary_tree_t *tmp[1024], *cur;
 
-if (tree == NULL)
-return (0);
+if (!tree) return (0);
 
-/* Allocate memory for queue (assuming max 1024 nodes) */
+/* Count nodes using stack */
+tmp[0] = tree;
+while (count < 1024)
+{
+cur = tmp[count];
+if (!cur) break;
+tmp[++count] = cur->left;
+tmp[++count] = cur->right;
+}
+
 queue = malloc(sizeof(binary_tree_t *) * 1024);
-if (queue == NULL)
-return (0);
+if (!queue) return (0);
 
-queue[rear++] = tree;
-
+queue[rear++] = (binary_tree_t *)tree;
 while (front < rear)
 {
-current = queue[front++];
+cur = queue[front++];
+if (cur->left)
+{
+if (flag) { free(queue); return (0); }
+queue[rear++] = (binary_tree_t *)cur->left;
+}
+else flag = 1;
 
-/* Check left child */
-if (current->left != NULL)
+if (cur->right)
 {
-/* If we've already seen a non-full node, then this tree is not complete */
-if (flag)
-{
-free(queue);
-return (0);
+if (flag) { free(queue); return (0); }
+queue[rear++] = (binary_tree_t *)cur->right;
 }
-queue[rear++] = current->left;
+else flag = 1;
 }
-else
-{
-flag = 1; /* Mark that we've seen a node without left child */
-}
-
-/* Check right child */
-if (current->right != NULL)
-{
-/* If we've already seen a non-full node, then this tree is not complete */
-if (flag)
-{
-free(queue);
-return (0);
-}
-queue[rear++] = current->right;
-}
-else
-{
-flag = 1; /* Mark that we've seen a node without right child */
-}
-}
-
 free(queue);
 return (1);
 }
